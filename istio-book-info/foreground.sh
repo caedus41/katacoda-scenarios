f213d9d3b7294a2613edf0ada9f7085df9652b19
Chris Thompson <cthompson@oreilly.com>
2020-09-14 16:58:38 -0400

    add  sleep

--- istio-book-info/foreground.sh
+++ istio-book-info/foreground.sh
@@ -1,5 +1,6 @@
- kubectl  create ns book-info
- kubectl apply -n book-info -f /root/reviews.yaml
- kubectl apply -n book-info -f /root/productinfo.yaml
- kubectl apply -n book-info -f /root/ratings.yaml
- kubectl apply -n book-info -f /root/details.yaml
+sleep 10
+kubectl  create ns book-info
+kubectl apply -n book-info -f /root/reviews.yaml
+kubectl apply -n book-info -f /root/productinfo.yaml
+kubectl apply -n book-info -f /root/ratings.yaml
+kubectl apply -n book-info -f /root/details.yaml
